d32234439fe8fc0e54ec6a663c4366a35c13d87d
legionJP <jjpsingh18@gmail.com>
2022-09-14 19:38:28 +0530

    commiting all the changes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,16 +1,37 @@
-from flask import session, redirect, url_for, render_template, request
-from . import main
-from .forms import LoginForm
+from fileinput import close
+from unicodedata import name
+#event import
+from flask import session, Flask,redirect, url_for, render_template, request
 
+from app.main import db
+from . import main
+from app import create_app, socketio
+from .forms import LoginForm
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQLdb ,MySQL
+app= Flask('__name__')
+mysql = MySQL(app)
+db = MySQL(app)
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = 'JPsdatabase@23'
+#app.config['MYSQL_DB'] = 'my_db'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql:///root:JPsdatabase@23@localhost/jp_db'
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
+     
+     
     """Login form to enter a room."""
-    form = LoginForm()
+    form = LoginForm()    
     if form.validate_on_submit():
         session['name'] = form.name.data
-        session['room'] = form.room.data
+        session['room'] = form.room.data        
         return redirect(url_for('.chat'))
+     
+     
+
     elif request.method == 'GET':
         form.name.data = session.get('name', '')
         form.room.data = session.get('room', '')
@@ -19,10 +40,20 @@ def index():
 
 @main.route('/chat')
 def chat():
+     
     """Chat room. The user's name and room must be stored in
     the session."""
+         
     name = session.get('name', '')
     room = session.get('room', '')
     if name == '' or room == '':
         return redirect(url_for('.index'))
+
     return render_template('chat.html', name=name, room=room)
+        
+
+    #name = session.get('name', '')
+    #room = session.get('room', '')
+    #if name == '' or room == '':
+    #   return redirect(url_for('.index'))
+    #return render_template('chat.html', name=name, room=room)
